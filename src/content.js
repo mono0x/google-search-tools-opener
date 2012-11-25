e23ce291992a18fb1d94b4349b5b5d6ce92d59b2
mono <mono@monoweb.info>
2012-11-25 16:50:48 +0900

    Automatically open dropdown menus

--- src/content.js
+++ src/content.js
@@ -1,5 +1,5 @@
-var element = document.getElementById('hdtbMenus');
-element.setAttribute('class', 'hdtb-td-o');
+var menus = document.getElementById('hdtbMenus');
+menus.setAttribute('class', 'hdtb-td-o');
 
 var style = document.createElement('style');
 style.setAttribute('type', 'text/css');
@@ -8,3 +8,41 @@ var stylesheet = document.styleSheets[0];
 
 stylesheet.addRule('#hdtb_tls', 'display: none');
 stylesheet.addRule('#hdtbMenus', 'position: static !important');
+
+Array.prototype.forEach.call(menus.querySelectorAll('.mn-hd-txt, .mn-dwn-arw'), function(t) { 
+  t.addEventListener('mouseover', function(e) {
+    var target = t.parentNode;
+    var element;
+    for (element = e.target; element; element = element.parentNode) {
+      if (element == menus) {
+        if (/hdtb-mn-c/.test(target.nextSibling.className)) {
+          t.click();
+        }
+        return;
+      }
+    }
+  }, false);
+
+  t.addEventListener('mouseout', function(e) {
+    var target = t.parentNode.nextSibling;
+    var element;
+    for (element = e.relatedTarget; element; element = element.parentNode) {
+      if (element == target) {
+        return;
+      }
+    }
+    t.click();
+  });
+});
+
+Array.prototype.forEach.call(document.querySelectorAll('.hdtbU'), function(t) {
+  t.addEventListener('mouseout', function(e) {
+    var element;
+    for (element = e.relatedTarget; element; element = element.parentNode) {
+      if (element == t) {
+        return;
+      }
+    }
+    t.previousSibling.firstChild.click();
+  }, false);
+});
