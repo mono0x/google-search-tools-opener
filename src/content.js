4c37a6d7c85685693de0ebc2e1020b15ecbcf777
mono <mono@monoweb.info>
2012-11-25 15:42:26 +0900

    Show the number of search results under the search tools

--- src/content.js
+++ src/content.js
@@ -1,2 +1,10 @@
 var element = document.getElementById('hdtbMenus');
 element.setAttribute('class', 'hdtb-td-o');
+
+var style = document.createElement('style');
+style.setAttribute('type', 'text/css');
+document.getElementsByTagName('head')[0].appendChild(style);
+var stylesheet = document.styleSheets[0];
+
+stylesheet.addRule('#hdtb_tls', 'display: none');
+stylesheet.addRule('#hdtbMenus', 'position: static !important');
