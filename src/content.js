009108ba82c477675dc52abdc25d8a3f4b1490c5
mono <mono@monoweb.info>
2013-01-14 20:30:42 +0900

    Update for Google

--- src/content.js
+++ src/content.js
@@ -8,6 +8,8 @@ var stylesheet = document.styleSheets[0];
 
 stylesheet.addRule('#hdtb_tls', 'display: none');
 stylesheet.addRule('#hdtbMenus', 'position: static !important');
+stylesheet.addRule('#resultStats', 'opacity: 1 !important');
+stylesheet.addRule('#resultStats', 'top: 0 !important');
 
 Array.prototype.forEach.call(menus.querySelectorAll('.mn-hd-txt, .mn-dwn-arw'), function(t) { 
   t.addEventListener('mouseover', function(e) {
